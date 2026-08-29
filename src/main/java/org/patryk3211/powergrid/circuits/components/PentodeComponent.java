4b3d34bba7f5ffa40f0dfb3c27d2d2e33d3593f5
mechspace2 <61834471+mechspace2@users.noreply.github.com>
2026-08-29 23:32:50 +0200

    Align the default component parameters with more like a EL84/6BQ5 pentode, and try to do the redplating stuff here too

--- src/main/java/org/patryk3211/powergrid/circuits/components/PentodeComponent.java
+++ src/main/java/org/patryk3211/powergrid/circuits/components/PentodeComponent.java
@@ -17,9 +17,6 @@ package org.patryk3211.powergrid.circuits.components;
 
 import com.google.common.collect.ImmutableCollection;
 import com.mojang.blaze3d.vertex.PoseStack;
-import com.simibubi.create.foundation.render.RenderTypes;
-import net.createmod.catnip.render.CachedBuffers;
-import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.util.Mth;
@@ -38,18 +35,19 @@ import org.patryk3211.powergrid.electricity.base.ThermalBehaviour;
 import org.patryk3211.powergrid.electricity.sim.special.PentodeWire;
 
 public class PentodeComponent extends MirrorableComponent implements IRenderedComponent {
-    public static final FloatProperty GAIN = new FloatProperty(PowerGrid.MOD_ID, "tube_gain", 8, 1, 100);
-    public static final FloatProperty K_G = new FloatProperty(PowerGrid.MOD_ID, "tube_kg", 6_000, 200, 20_000);
+    // EL84
+    public static final FloatProperty GAIN = new FloatProperty(PowerGrid.MOD_ID, "tube_gain", 19.3f, 1, 100);
+    public static final FloatProperty K_G = new FloatProperty(PowerGrid.MOD_ID, "tube_kg", 538, 200, 20_000);
     public static final FloatProperty K_G2 = new FloatProperty(PowerGrid.MOD_ID, "tube_kg2", 4_500, 200, 20_000);
-    public static final FloatProperty K_P = new FloatProperty(PowerGrid.MOD_ID, "tube_kp", 48, 10, 2000);
-    public static final FloatProperty K_VB = new FloatProperty(PowerGrid.MOD_ID, "tube_kvb", 12, 1, 1000);
+    public static final FloatProperty K_P = new FloatProperty(PowerGrid.MOD_ID, "tube_kp", 74, 10, 2000);
+    public static final FloatProperty K_VB = new FloatProperty(PowerGrid.MOD_ID, "tube_kvb", 17.9f, 1, 1000);
     public static final FloatProperty EX = new FloatProperty(PowerGrid.MOD_ID, "tube_ex", 1.35f, 1.2f, 1.6f);
-    public static final FloatProperty SATURATION_CURRENT = new FloatProperty(PowerGrid.MOD_ID, "tube_saturation_current", 0.1f, 0.001f, 20);
-    public static final FloatProperty HEATER_VOLTAGE = new FloatProperty(PowerGrid.MOD_ID, "tube_heater_voltage", 6f, 1f, 16f);
-    public static final CalculatedProperty<Float> HEATER_POWER = new CalculatedProperty<>(PowerGrid.MOD_ID, "tube_heater_power", state -> {
-        var Is = state.get(SATURATION_CURRENT);
-        return Math.max(5f, Is * 50f);
-    }, value -> String.format("%.1f W", value));
+    public static final FloatProperty SATURATION_CURRENT = new FloatProperty(PowerGrid.MOD_ID, "tube_saturation_current", 0.065f, 0.001f, 20);
+    public static final FloatProperty HEATER_VOLTAGE = new FloatProperty(PowerGrid.MOD_ID, "tube_heater_voltage", 6.3f, 1f, 16f);
+    public static final CalculatedProperty<Float> HEATER_POWER = new CalculatedProperty<>(PowerGrid.MOD_ID, "tube_heater_power",
+            state -> 4.8f,
+            value -> String.format("%.1f W", value));
+    public static final FloatProperty PLATE_DISSIPATION = new FloatProperty(PowerGrid.MOD_ID, "tube_plate_dissipation", 12f, 0.25f, 100);
 
     public PentodeComponent(ComponentFootprint footprint) {
         super(footprint);
@@ -58,7 +56,7 @@ public class PentodeComponent extends MirrorableComponent implements IRenderedCo
     @Override
     protected void addProperties(ImmutableCollection.Builder<ComponentProperty<?>> properties) {
         super.addProperties(properties);
-        properties.add(GAIN, K_G, K_G2, K_P, K_VB, EX, SATURATION_CURRENT, HEATER_VOLTAGE, HEATER_POWER);
+        properties.add(GAIN, K_G, K_G2, K_P, K_VB, EX, SATURATION_CURRENT, HEATER_VOLTAGE, HEATER_POWER, PLATE_DISSIPATION);
     }
 
     @Override
@@ -107,21 +105,12 @@ public class PentodeComponent extends MirrorableComponent implements IRenderedCo
                     data.prev = data.current;
                     data.current = Mth.clamp((temperature - 1000) / 400f, 0, 1.125f);
                 });
+
+        ElectronTubeComponent.addPlateThermal(thermals, tube, data, placed.get(PLATE_DISSIPATION));
     }
 
     @Override
     public void render(CircuitBoardBlockEntity be, PlacedComponent placed, float partialTicks, PoseStack ms, MultiBufferSource bufferSource, int light, int overlay) {
-        int a = 0;
-        if (placed.customData instanceof ElectronTubeComponent.RenderData data) {
-            a = (int) (Mth.lerp(partialTicks, data.prev, data.current) * 170);
-        }
-        if (a == 0)
-            return;
-        var buffer = CachedBuffers.partial(ModdedPartialModels.PENTODE_GLOW, be.getBlockState());
-        buffer
-                .disableDiffuse()
-                .color(a, a, a, 255)
-                .light(LightTexture.FULL_BRIGHT)
-                .renderInto(ms, bufferSource.getBuffer(RenderTypes.additive()));
+        ElectronTubeComponent.renderTubeGlow(be, placed, partialTicks, ms, bufferSource, ModdedPartialModels.PENTODE_GLOW, 170);
     }
 }
